a7cc6eb789f335b48b986dab436e6b5a4ad2d1b3
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-02-21 22:39:30 +0600

    new commit

---
+++ mad_libs_generator.py
@@ -0,0 +1,7 @@
+username = input("what is your name?")
+fathername = input("what is your father name?")
+mothername = input("what is your mother name?")
+study = input("which class do you study?")
+relationship = input("tell me about your relationship status?")
+
+print(f"My doggy name is {username}, my doggy's father name {fathername}, my dogg's mother name {mothername}. my doggy study in class {study}, and our relationship is {relationship}")
